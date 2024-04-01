d7705c68f892865eb8b3b3c4b519433a6bb39712
GuiWar <120386099+GuiWar@users.noreply.github.com>
2024-04-01 12:10:59 +0000

    Update home.ts
    removed additional grid details

--- client/src/routes/home/home.ts
+++ client/src/routes/home/home.ts
@@ -1,8 +1,6 @@
 import { customElement, FASTElement } from '@microsoft/fast-element';
 import { HomeStyles as styles } from './home.styles';
-import { HomeTemplate as template } from './home.template';
-import {DataGrid} from '@genesislcap/foundation-zero';
-import { getActionsMenuDef, GridProRendererTypes } from '@genesislcap/grid-pro';
+import { HomeTemplate as template } from './home.template';´
 
 
 
@@ -14,52 +12,5 @@ import { getActionsMenuDef, GridProRendererTypes } from '@genesislcap/grid-pro';
 
 
 export class Home extends FASTElement {
-
-
-    customLogLevelCellRenderer = (params) => {
-      return `<span style="color: green">Custom ${params.color}</span>`;
-      }
-      public myActionsMenuColDef = getActionsMenuDef(
-        [
-          {
-            name: 'View',
-            callback: rowData => console.log('VIEWW!!!', rowData),
-          },
-          {
-            name: 'Delete',
-            callback: rowData => console.log('DELETE!!!', rowData),
-          },
-        ],
-        {
-          headerName: 'Test Actions',
-          width: 140,
-          pinned: 'right'
-        },
-        '+',
-      );
-
-  public multipleCustomColumnConfigArray: ColDef[] = [
-    {
-      headerName: 'Is Active',
-      field: 'IS_ACTIVE',
-      pinned: "right"
-    },
-    {
-      headerName: 'Custom ',
-      width: 100,
-      pinned: "right",
-      cellRenderer: this.customLogLevelCellRenderer,
-      cellRendererParams: {color: 'red'}
-    }
-  ];
-
-public myCustomColumn: ColDef =[
-  {
-    headerName: 'Custom Name',
-    pinned: 'right',
-    cellRenderer: GridProRendererTypes.boolean,
-    editable: true
-  }]
-;
 
 }
